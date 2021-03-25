cec8be736bcb36cf234ce0e690c19651dacae1ca
Edwin Wong <edwinwong@google.com>
2021-03-25 14:40:00 -0700

    universal3475-common: update clearkey plugin to DRM HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I5b2da69e38450e130a636272c6ebfa6684ff1042
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -80,7 +80,7 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-	android.hardware.drm@1.3-service.clearkey
+	android.hardware.drm@1.4-service.clearkey
 
 # DTB
 PRODUCT_HOST_PACKAGES += \
