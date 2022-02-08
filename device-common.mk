ebb01ebcde9a7af846959f5919f822ac1a60e368
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    universal3475-common: drop audio.a2dp.default
    - building from here
      https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device-common.mk
+++ device-common.mk
@@ -23,7 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audio.a2dp.default \
     audio.primary.universal3475 \
     audio.r_submix.default \
     audio.usb.default \
