67d6d7b4a1f4c2265745198c47d5ccefe621191d
lavenly <135024283+lavenly@users.noreply.github.com>
2026-06-29 03:47:44 -0700

    universal3475-common: use lazy-loading variant to fix Wi-Fi HAL race condition

--- device-common.mk
+++ device-common.mk
@@ -309,7 +309,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0 \
     android.hardware.wifi@1.0-impl \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service-lazy \
     hostapd \
     libwpa_client \
     macloader \
