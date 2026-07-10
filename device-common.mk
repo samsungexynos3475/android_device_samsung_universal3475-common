b3511acbb2b684fe62e710b29c37f4a9aeece3bf
lavenly <135024283+lavenly@users.noreply.github.com>
2026-07-10 00:55:11 -0700

    universal3475-common: switch power HAL to samsung-libperfmgr v1.3

--- device-common.mk
+++ device-common.mk
@@ -158,7 +158,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos
+    android.hardware.power@1.3-service.samsung-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Permissions
 PRODUCT_COPY_FILES += \
