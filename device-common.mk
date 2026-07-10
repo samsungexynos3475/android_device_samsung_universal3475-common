792edd4e63b69fd142d562a02682b12c19f30b67
lavenly <135024283+lavenly@users.noreply.github.com>
2026-07-10 02:39:39 -0700

    universal3475-common: switch Samsung libperfmgr HAL to AIDL

--- device-common.mk
+++ device-common.mk
@@ -157,7 +157,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.samsung-libperfmgr
+    android.hardware.power-service.samsung-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -271,6 +271,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
 	$(COMMON_PATH) \
+    hardware/google/pixel \
+	hardware/google/interfaces \
+	hardware/samsung/aidl/power-libperfmgr \
 	hardware/samsung_slsi-linaro/exynos/cpboot
 
 # TextClassifier
