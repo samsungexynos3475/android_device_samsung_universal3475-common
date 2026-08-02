e6da311151e46a35ce7289e22b0c990eb44003eb
lavenly <135024283+lavenly@users.noreply.github.com>
2026-08-02 04:05:58 -0700

    universal3475-common: use backwards compatible graphic mapper HAL v2.1

--- device-common.mk
+++ device-common.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
 
 # Health
 PRODUCT_PACKAGES += \
