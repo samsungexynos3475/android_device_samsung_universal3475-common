c50f6c105b26d14a7697e8c03e34219154a8a9d0
lavenly <135024283+lavenly@users.noreply.github.com>
2026-08-03 05:48:21 -0700

    universal3475-common: use non legacy camera provider impl HAL

--- device-common.mk
+++ device-common.mk
@@ -57,7 +57,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.universal3475 \
     Snap
