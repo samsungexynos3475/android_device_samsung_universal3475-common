5da37b2a272f34dca96c53c471f7e230c5fb69ad
lavenly <135024283+lavenly@users.noreply.github.com>
2026-08-03 20:16:26 -0700

    universal3475-common: update clearkey plugin to DRM HAL v1.3

--- device-common.mk
+++ device-common.mk
@@ -77,7 +77,7 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-	android.hardware.drm@1.2-service.clearkey
+	android.hardware.drm@1.3-service.clearkey
 
 # DTB
 PRODUCT_HOST_PACKAGES += \
